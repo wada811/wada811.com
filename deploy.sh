4e8a86758313cb1b2f287412c6d561706d44be87
wada811 <at.wada811@gmail.com>
2015-03-03 22:01:27 +0900

    git submodule add git@github.com:wada811/wada811.github.io.git public

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Refresh Public folder
-rm -rf public
-mkdir public
-echo "gitdir: ../.git/modules/public" > public/.git
-echo "blog.wada811.com" > public/CNAME
-
 # Build the project. 
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
