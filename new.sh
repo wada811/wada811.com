235537a748e5faba17122bbbf833519cfe908f05
wada811 <at.wada811@gmail.com>
2015-08-20 21:51:22 +0900

    Fix new.sh

--- new.sh
+++ new.sh
@@ -5,5 +5,10 @@ if [ $# -eq 1 ]
 then
     hugo new "post/$1.md"
     subl "content/post/$1.md"
+    # Go To Public folder
+    cd public
+    git commit --allow-empty -m "Write: $1"
+    # Come Back
+    cd ..
     git commit --allow-empty -m "Write: $1"
 fi
